21a32ddba4a33db21c8524e12ae8ec0040bf1c30
Robert Sterry-Kimmitt <rakimmitt@hotmail.com>
2026-09-07 12:44:59 +0100

    Update CPRDData environment for diabetes cohort

--- CPRD-RobertK-CKD-cohort/Prevalent cohort (dm)/00 all_patid death causes dm.R
+++ CPRD-RobertK-CKD-cohort/Prevalent cohort (dm)/00 all_patid death causes dm.R
@@ -16,7 +16,7 @@ library(tidyverse)
 library(aurum)
 rm(list=ls())
 
-cprd = CPRDData$new(cprdEnv = "nondiabetes-jun2024",cprdConf = "~/.aurum.yaml")
+cprd = CPRDData$new(cprdEnv = "diabetes-jun2024",cprdConf = "C:\\Users\\rk535\\OneDrive\\1 - PhD\\Data Science\\CPRD\\.aurum.yaml")
 codesets = cprd$codesets()
 codes = codesets$getAllCodeSetVersion(v = "01/06/2024")
 
